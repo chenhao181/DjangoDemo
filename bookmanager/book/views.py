d971282449932b3edca5f4010a76024c47bc196e
ch <1253921036@qq.com>
2023-03-24 21:59:35 +0800

    '模板'

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('...')
+    return render(request, 'book/index.html')
--- bookmanager/bookmanager/settings.py
+++ bookmanager/bookmanager/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -52,10 +52,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'bookmanager.urls'
 
+# templates配置文件
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # 告知系统我们的模板文件放在哪里
+        'DIRS': [os.path.join(BASE_DIR, 'templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
