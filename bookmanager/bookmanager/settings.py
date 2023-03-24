221c55f7ef78eb75bdefa9d534e1582d066daa24
ch <1253921036@qq.com>
2023-03-24 22:25:30 +0800

    '静态资源设置和访问'

--- bookmanager/bookmanager/settings.py
+++ bookmanager/bookmanager/settings.py
@@ -120,7 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+# 告诉系统我们的图片在哪
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
