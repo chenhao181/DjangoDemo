2bf530b9cd973133203ae21343ab69d5f05b0536
ch <1253921036@qq.com>
2023-03-25 14:48:31 +0800

    '数据库操作'

--- bookmanager02/book/models.py
+++ bookmanager02/book/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
 # Create your models here.
+
+
+class BookInfo(models.Model):
+    name = models.CharField(max_length=10, unique=True, verbose_name='名字')
+    pub_date = models.DateField(null=True)
+    readCount = models.IntegerField(default=0)
+    commentCount = models.IntegerField(default=0)
+    is_delete = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'bookinfo'
+        verbose_name = '书籍管理'
+
+    def __str__(self):
+        return self.name
+
+
+class PeopleInfo(models.Model):
+    GENDER_CHOICE = (
+        (1, 'male'),
+        (2, 'female'),
+    )
+    name = models.CharField(max_length=10, unique=True)
+    gender = models.SmallIntegerField(choices=GENDER_CHOICE, default=1)
+    descriptions = models.CharField(max_length=100, null=True)
+    is_delete = models.BooleanField(default=False)
+
+    # 外键
+    # 系统会自动为外键添加 _id
+    book = models.ForeignKey(BookInfo, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'peopleinfo'
+
+    def __str__(self):
+        return self.name
+
+
+
--- bookmanager02/bookmanager02/settings.py
+++ bookmanager02/bookmanager02/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'book.apps.BookConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'bookmanager02.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'PORT': 3306,
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'NAME': 'book_01',  # 数据库
     }
 }
 
